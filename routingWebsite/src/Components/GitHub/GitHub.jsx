812908d810dec00cc1268d97c6bd65a3355d6284
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-01-17 00:35:23 +0530

    Application Pages Redering Complited

---
+++ routingWebsite/src/Components/GitHub/GitHub.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react"
+
+
+function GitHub() {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.github.com/users/manitejagaddam")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            setData(data);
+            
+        })
+    }, [])
+
+
+
+  return (
+    <div className="flex items-center justify-center">
+        <div className="p-5 text-3xl text-center text-white bg-zinc-400"> GitHub UserName : {data.login} No of Repositories : {data.public_repos}</div>
+
+        <img src={data.avatar_erl} alt="Git Hub Profile Page" width={300}/>
+
+    </div>
+  )
+}
+
+export default GitHub
