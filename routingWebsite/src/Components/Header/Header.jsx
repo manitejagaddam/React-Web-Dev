ab42eb89ef68b9a88b286545706737467c2c7993
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-01-17 00:59:39 +0530

    Full Application Done

--- routingWebsite/src/Components/Header/Header.jsx
+++ routingWebsite/src/Components/Header/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
                         >
                             Get started
                         </Link>
-                    </div>
+                    </div>  
                     <div
                         className="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1"
                         id="mobile-menu-2"
@@ -35,11 +35,42 @@ export default function Header() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
-                                    className={() =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                to={"/"}
+                                    className={(isActive) =>
+                                        `${isActive ? "text-orange-700" : "text-gray-700"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                to={"/about"}
+                                    className={(isActive) =>
+                                        `${isActive ? "text-orange-700" : "text-gray-700"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    About Us
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                to={"/contact-us"}
+                                    className={(isActive) =>
+                                        `${isActive ? "text-orange-700" : "text-gray-700"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    Contact Us
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to={"/github"}
+                                    className={(isActive) =>
+                                        `${isActive ? "text-orange-700" : "text-gray-700"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    Git Hub
                                 </NavLink>
                             </li>
                             
